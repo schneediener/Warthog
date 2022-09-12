e1c0a1bbd78b57bd5b4d1a97f4e990d3d2cca2e2
Sean <schneediener@gmail.com>
2022-09-12 17:06:39 +0930

    bug fix

--- game.gd
+++ game.gd
@@ -29,7 +29,7 @@ func _on_spawn_timer_timeout():
 
 	randomize()
 	var spawn_point = randi() % 6
-	var spawn_array = $spawn_container.get_children()
+	var spawn_array = $KinematicBody2D/spawn_container.get_children()
 	enemy_instance.global_position = spawn_array[spawn_point].global_position
 	$enemy_container.add_child(enemy_instance)
 	
