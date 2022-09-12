695987195e181e3fff31f43cfa413c46e66497b2
Sean <schneediener@gmail.com>
2022-09-12 14:09:26 +0930

    removal of silly bullet sprite

--- enemy.gd
+++ enemy.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var type = "enemy"
 var subtype = "creep"
+var velocity
+var speed = 100
 onready var player = get_node("/root/Node2D/KinematicBody2D")
 onready var remaining_dist = global_position.distance_to(player.global_position)
 # Declare member variables here. Examples:
@@ -14,7 +16,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	remaining_dist = global_position.distance_to(player.global_position)
+	velocity = Vector2.ZERO
+	if player:
+		velocity = position.direction_to(player.position) * speed
+	velocity = move_and_slide(velocity)
 
 func get_distance():
 	return remaining_dist
