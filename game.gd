97be390f66af07639dc4f2fb982eb95a39d21971
Sean <schneediener@gmail.com>
2022-09-12 17:03:12 +0930

    basic tilemap + collision

--- game.gd
+++ game.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_camera_limits()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +16,13 @@ func _process(delta):
 	if $TextureProgress.value != $KinematicBody2D.current_health:
 		$TextureProgress.value=$KinematicBody2D.current_health
 
+func set_camera_limits():
+	var map_limits = $TileMap.get_used_rect()
+	var map_cellsize = $TileMap.cell_size
+	$KinematicBody2D/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
+	$KinematicBody2D/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
+	$KinematicBody2D/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
+	$KinematicBody2D/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
 
 func _on_spawn_timer_timeout():
 	var enemy_instance = load("res://enemy.tscn").instance()
